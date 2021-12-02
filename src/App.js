d632141a82f363a5aff4bd0812cc36f2b9d83e16
Leo <leolair.pro@gmail.com>
2021-12-02 18:49:44 +0100

    for heroku

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import {BrowserRouter as BrowserRouter, Route, Routes} from 'react-router-dom'
 import LoginOrRegister from './pages/LoginOrRegister';
@@ -7,35 +6,20 @@ import Register from './pages/Register';
 import RegisterStepTwo from './pages/RegisterStepTwo';
 import Discover from './pages/Discover';
 import TabBar from './components/TabBar';
-import Search from './pages/Search';
-import Message from './pages/Message';
-import Profile from './pages/Profile';
-
-function displayTabBar() {
-    if (document.location.href == "http://localhost:3000/" || document.location.href == "http://localhost:3000/login" || document.location.href == "http://localhost:3000/register" || document.location.href == "http://localhost:3000/registertwo") {
-      return
-    }else{
-      return <TabBar />
-    }
-}
 
 function App() {
-  console.log(document.location.href);
   return (
     <div className="App">
       <BrowserRouter>
           <Routes>
-              <Route path="https://lehoax.github.io/Share/" exact element={<LoginOrRegister/>}/>
-              <Route path="https://lehoax.github.io/Share/login" exact element={<Login/>}/>
-              <Route path="https://lehoax.github.io/Share/register" exact element={<Register/>}/>
-              <Route path="https://lehoax.github.io/Share/registertwo" exact element={<RegisterStepTwo/>}/>
-              <Route path="https://lehoax.github.io/Share/discover" exact element={<Discover/>}/>
-              <Route path="https://lehoax.github.io/Share/search" exact element={<Search/>}/>
-              <Route path="https://lehoax.github.io/Share/message" exact element={<Message/>}/>
-              <Route path="https://lehoax.github.io/Share/profile" exact element={<Profile/>}/>
+              <Route path="/" exact element={<LoginOrRegister/>}/>
+              <Route path="/login" exact element={<Login/>}/>
+              <Route path="/register" exact element={<Register/>}/>
+              <Route path="/registertwo" exact element={<RegisterStepTwo/>}/>
+              <Route path="/discover" exact element={<Discover/>}/>
           </Routes>
         </BrowserRouter>
-        {displayTabBar()}
+        <TabBar />
     </div>
   );
 }
