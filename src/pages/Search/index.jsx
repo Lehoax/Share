383bb477afb5287205ac2682e4e24546d33fb460
Leo <leolair.pro@gmail.com>
2021-12-06 03:35:01 +0100

    fix prod bug webpack

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css"
-import SearchResult from "../../components/SearchResult"
+import SearchResult from "../../components/SearchResultat"
 require('dotenv').config();
 
 class Search extends React.Component {
