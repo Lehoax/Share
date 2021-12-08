512d67fabeb2bcdf519043f60abb7ef903526bf9
Leo <leolair.pro@gmail.com>
2021-12-08 16:27:07 +0100

    Updates

---
+++ precache-manifest.2bf22c2e7c446c3456afbcfde170fb22.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219fd8a17bacc4de5a394775c7a4d7fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b84983f6fc88ee1c9629",
+    "url": "/static/css/2.6879db70.chunk.css"
+  },
+  {
+    "revision": "b42b406ece0414a90790",
+    "url": "/static/css/main.025528b8.chunk.css"
+  },
+  {
+    "revision": "b84983f6fc88ee1c9629",
+    "url": "/static/js/2.f1560fff.chunk.js"
+  },
+  {
+    "revision": "3035b3796dc89ab06c0eab01a3f8b30b",
+    "url": "/static/js/2.f1560fff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249343a67ed0064466b4",
+    "url": "/static/js/3.d82e90d8.chunk.js"
+  },
+  {
+    "revision": "b42b406ece0414a90790",
+    "url": "/static/js/main.c8906718.chunk.js"
+  },
+  {
+    "revision": "6f4f0cc5af6b1763c6cc",
+    "url": "/static/js/runtime-main.9ee274a3.js"
+  },
+  {
+    "revision": "b8982e196d7c86f12143ffa41d2b33a8",
+    "url": "/static/media/1077063.b8982e19.png"
+  },
+  {
+    "revision": "9da111c9b37d0f31a03672009949ebeb",
+    "url": "/static/media/2462719.9da111c9.png"
+  },
+  {
+    "revision": "740fdfa153b9500eb4cbcda500772adf",
+    "url": "/static/media/2997940.740fdfa1.png"
+  },
+  {
+    "revision": "6b5b86d16707b4fc030c01fa3ff2804c",
+    "url": "/static/media/846449.6b5b86d1.png"
+  }
+]);
